56c8c258bff6804a7a74fa8bc4c3f45a18d9029b
shadowwzw <miditab@yandex.ru>
2017-03-30 23:15:28 +0400

    rewrote server on fs-promise

--- gulpfile.js
+++ gulpfile.js
@@ -5,41 +5,42 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 gulp.task('server', cb => {
-    pump([
-            gulp.src('src/server.js'),
-            eslint(),
-            eslint.format(),
-            eslint.failAfterError(),
-            babel({
-                presets: ['es2015'],
-                plugins: [
-                    require('babel-plugin-transform-object-rest-spread'),
-                    require('babel-plugin-transform-class-properties'),
-                    require('babel-plugin-transform-async-to-generator'),
-                    require('babel-plugin-transform-runtime'),
-                ]
-            }),
-            gulp.dest('lib')],
-        cb);
+  pump([
+      gulp.src('src/server.js'),
+      eslint(),
+      eslint.format(),
+      eslint.failAfterError(),
+      babel({
+        presets: ['es2015'],
+        plugins: [
+          require('babel-plugin-transform-object-rest-spread'),
+          require('babel-plugin-transform-class-properties'),
+          require('babel-plugin-transform-async-to-generator'),
+          require('babel-plugin-transform-runtime'),
+        ]
+      }),
+      gulp.dest('lib')],
+    cb);
 });
 
 gulp.task('client', cb => {
-    pump([
-            gulp.src('src/client.js'),
-            eslint(),
-            eslint.format(),
-            eslint.failAfterError(),
-            babel({
-                presets: ['es2015'],
-                plugins: [
-                    require('babel-plugin-transform-object-rest-spread'),
-                    require('babel-plugin-transform-class-properties'),
-                    require('babel-plugin-transform-async-to-generator'),
-                ]
-            }),
-            uglify(),
-            gulp.dest('lib')],
-        cb);
+  pump([
+      gulp.src('src/client.js'),
+      eslint(),
+      eslint.format(),
+      eslint.failAfterError(),
+      babel({
+        presets: ['es2015'],
+        plugins: [
+          require('babel-plugin-transform-object-rest-spread'),
+          require('babel-plugin-transform-class-properties'),
+          require('babel-plugin-transform-async-to-generator'),
+          require('babel-plugin-transform-runtime'),
+        ]
+      }),
+      uglify(),
+      gulp.dest('lib')],
+    cb);
 });
 
 gulp.task('build', ['server', 'client']);
