6583a35189ef575ad2f035d98416e75e7066f393
shadowwzw <miditab@yandex.ru>
2017-03-31 11:03:48 +0400

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ const pump = require('pump');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const gulpif = require('gulp-if');
 
-gulp.task('server', cb => {
+gulp.task('build', cb => {
   pump([
-      gulp.src('src/server.js'),
+      gulp.src('src/*.js'),
       eslint(),
       eslint.format(),
       eslint.failAfterError(),
@@ -19,28 +20,7 @@ gulp.task('server', cb => {
           require('babel-plugin-transform-runtime'),
         ]
       }),
+      gulpif('client.js', uglify()),
       gulp.dest('lib')],
     cb);
 });
-
-gulp.task('client', cb => {
-  pump([
-      gulp.src('src/client.js'),
-      eslint(),
-      eslint.format(),
-      eslint.failAfterError(),
-      babel({
-        presets: ['es2015'],
-        plugins: [
-          require('babel-plugin-transform-object-rest-spread'),
-          require('babel-plugin-transform-class-properties'),
-          require('babel-plugin-transform-async-to-generator'),
-          require('babel-plugin-transform-runtime'),
-        ]
-      }),
-      uglify(),
-      gulp.dest('lib')],
-    cb);
-});
-
-gulp.task('build', ['server', 'client']);
